2c05994706c0b8c546b194c3fb87825034f3a182
Atle Brandt <845667+atleb@users.noreply.github.com>
2024-12-01 23:43:21 +0100

    chore: update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import netlify from "@astrojs/netlify/edge-functions";
 
 // read more at doc site: https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  output: 'hybrid',
-  experimental: {
-    hybridOutput: true,
-  },
-  adapter: netlify()
+  output: "server",
+  adapter: netlify({
+    imageCDN: false,
+    cacheOnDemandPages: true, // default to up to one year, set headers to specify
+  }),
+  // sample headers, add to each PAGE:
+  // Astro.response.headers.set('CDN-Cache-Control', 'public, max-age=300, must-revalidate');
+  //  ---
+  /* copilot suggested addon; 
+    functions: {
+      "api/*": {
+        src: "src/functions/api.js",
+      },
+    }, */
 });
