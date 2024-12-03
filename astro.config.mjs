cf2e4ff5f29fcd406d240fc07ce58dcfd9f0a6c4
atleb <845667+atleb@users.noreply.github.com>
2024-12-03 16:56:30 +0100

    feat: Astro 5 (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,24 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify";
 
-// read more at doc site: https://astro.build/config
-export default defineConfig({
+//process.env.NODE_ENV -> development for run dev
+// adapt to dynamic set sutiable config, or improve local DX for netlify
+
+const prodConfig = {
   integrations: [vue()],
-  output: "hybrid",
+  output: "server",
   adapter: netlify({
     imageCDN: false,
     cacheOnDemandPages: true, // default to up to one year, set headers to specify
   }),
-  // sample headers, add to each PAGE:
-  // Astro.response.headers.set('CDN-Cache-Control', 'public, max-age=300, must-revalidate');
-  //  ---
-  /* copilot suggested addon; 
-    functions: {
-      "api/*": {
-        src: "src/functions/api.js",
-      },
-    }, */
-});
+};
+const devConfig = {
+  integrations: [vue()],
+};
+
+// read more at doc site: https://astro.build/config
+export default defineConfig(
+  prodConfig
+  //devConfig
+);
